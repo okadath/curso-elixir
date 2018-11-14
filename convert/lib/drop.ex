d9cd160ec26d8cf479518abde12864d988ad6d15
okadath <viruz_3107@hotmail.com>
2018-11-14 22:30:28 +0000

    estructura case

--- convert/lib/drop.ex
+++ convert/lib/drop.ex
@@ -20,4 +20,13 @@ mierdosa documentacion :v
         number
     end
 end
+
+#usando case
+def fall_velocity(planemo, distance) when distance >= 0 do
+case planemo do
+:earth -> :math.sqrt(2 * 9.8 * distance)
+:moon -> :math.sqrt(2 * 1.6 * distance)
+:mars -> :math.sqrt(2 * 3.71 * distance)
+end
+end
 end
