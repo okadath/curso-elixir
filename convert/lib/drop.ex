02c65d3d1466458235f13e0e22842ed9ba73b410
okadath <viruz_3107@hotmail.com>
2018-11-14 22:43:28 +0000

    case asign variable

--- convert/lib/drop.ex
+++ convert/lib/drop.ex
@@ -21,12 +21,24 @@ mierdosa documentacion :v
     end
 end
 
-#usando case
-def fall_velocity(planemo, distance) when distance >= 0 do
-case planemo do
-:earth -> :math.sqrt(2 * 9.8 * distance)
-:moon -> :math.sqrt(2 * 1.6 * distance)
-:mars -> :math.sqrt(2 * 3.71 * distance)
+#usando case para toda la funcion 
+    def fall_velocity2(planemo, distance) when distance >= 0 do
+        case planemo do
+        :earth -> :math.sqrt(2 * 9.8 * distance)
+        :moon -> :math.sqrt(2 * 1.6 * distance)
+        :mars -> :math.sqrt(2 * 3.71 * distance)
+        _ -> 1.0
+        end
+    end
+    #usando case para asignar una variable
+def fall_velocity3(planemo, distance) when distance >= 0 do
+gravity = case planemo do
+:earth -> 9.8
+:moon -> 1.6
+:mars -> 3.71
+_ -> 1
 end
+:math.sqrt(2 * gravity * distance)
 end
+
 end
