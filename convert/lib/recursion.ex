6199bbb24cc4ab667dfdfd6bdfc844f70b3ab9a7
okadath <viruz_3107@hotmail.com>
2018-11-16 17:17:31 +0000

    recursion whit return

--- convert/lib/recursion.ex
+++ convert/lib/recursion.ex
@@ -1,11 +1,27 @@
 defmodule Count do
-
+#lo normal es no usar nunca ifs, casi siempre guards
     def countdown(from) when from > 0 do
         IO.inspect(from)
         countdown(from-1)
     end
-    
     def countdown(from) do
         IO.puts("blastoff!")
     end
-end
+    
+    def factorial(n) do
+        factorial(1, n, 1)
+    end
+    
+    defp factorial(current, n, result) when current <= n do
+        new_result = result * current
+        IO.puts("#{current} yields #{new_result}.")
+        factorial(current + 1, n, new_result)
+    end
+    
+    defp factorial(_current, _n, result) do
+        IO.puts("finished!")
+        result
+    end
+
+
+end  
