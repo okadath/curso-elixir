914542cf9515fab7311a494f7720f95104b7e806
okadath <viruz_3107@hotmail.com>
2018-11-18 12:51:09 -0600

    maps

--- tests.exs
+++ tests.exs
@@ -52,7 +52,8 @@ end
 
 :math.sin 0
 :math.pow 2,17
-i "hello"
+#solo funciona en el interprete iex
+#i "hello"
 #metodos privados defp
 #anon func 
 fall_velocity= fn (distance) -> :math.sqrt(2 * 9.8 * distance) end
@@ -107,7 +108,7 @@ tuple_size(new_tuple)
 
 
 
-##me quede en los sigils
+##me quede en los sigils=========================
 
 #si uno usa el puts directamente decuele todo unido
 #en el iex si devuelve el array
@@ -285,18 +286,73 @@ HashDict.put(planemo_hash, :jupiter, 23.1)
 #si no existe nada nos lo indica al borrar
 HashDict.delete(planemo_hash, :jupiter)
 
-#mapas
+#mapas =================================================
 new_map = %{}
 number_map=%{2 => "two", 3 => "three"}
-IO.inspect number_map2=%{earth: 9.8, moon: 1.6}
+IO.inspect planemo_map = %{earth: 9.8, moon: 1.6, mars: 3.71}  
+#modificar, ambos funcionan igual
+%{planemo_map | earth: 12} 
+#basta con uno, si no existe manda error
+%{planemo_map | :earth => 12, :jup => 3} 
+#agregar
+Map.put_new(planemo_map, :jupiter, 23.1)
+#machear con una key, si no existe hay error
+%{earth: earth_gravity} = planemo_map
 
 
+#structs
+
+defprotocol Valid do
+@doc "Returns true if data is considered nominally valid"
+	def valid?(data)
+end
+
+defmodule Planemo do
+	defstruct name: :nil, 
+	gravity: 0, diameter: 0, distance_from_sun: 0
 
 
+end
+
+defimpl Valid, for: Planemo do
+	def valid?(p) do
+		p.gravity >= 0 and p.diameter >= 0 and
+		p.distance_from_sun >= 0
+	end
+end
+
+defmodule Tower do
+	defstruct location: "", 
+	height: 20, planemo: :earth, name: ""
+end
+
+tower5 = %Tower{planemo: :mars, height: 500, 
+name: "Daga Vallis", location: "Valles Marineris"}
+tower5.name
+%Tower{planemo: p, location: where} = tower5
 
 
+defmodule StructDrop do
+	#pasar un struct a una funcion, lo demas es como otras funciones
+	# def fall_velocity(t = %Tower{}) do
+	# fall_velocity(t.planemo, t.height)
+	# end
+	#lee los valores directamente del struct
+	def fall_velocity(%Tower{planemo: planemo, height: distance}) do
+		fall_velocity(planemo, distance)
+	end
+	def fall_velocity(:earth, distance) when distance >= 0 do
+		:math.sqrt(2 * 9.8 * distance)
+	end
+	def fall_velocity(:moon, distance) when distance >= 0 do
+		:math.sqrt(2 * 1.6 * distance)
+	end
+	def fall_velocity(:mars, distance) when distance >= 0 do
+		:math.sqrt(2 * 3.71 * distance)
+	end
+end
 
-
+StructDrop.fall_velocity(tower5)
 
 
 
