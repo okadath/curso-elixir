2d4ccb9328807f1d650d784b2278bd64d992c34f
okadath <viruz_3107@hotmail.com>
2018-11-23 17:33:19 +0000

    webserver python

--- asd.py
+++ asd.py
@@ -10,3 +10,6 @@ httpd = SocketServer.TCPServer(("", PORT), Handler)
 print "serving at port", PORT
 httpd.serve_forever()
 #python -m SimpleHTTPServer 8000
+#agregamos la variable y con la ventana abierta lo abrimos en otra ventana
+#luego lo corremos y copiamos la direccion en la que se despliega
+#aqui es http://node18.codenvy.io:56841/ no se si cambie, sospecho que si
